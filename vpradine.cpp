094651a58457592b300fd141f6d55a9e52b9c094
ArnasVoro <arnoldasvor@gmail.com>
2026-09-23 20:58:44 +0300

    pasirinkimas medianos, vidurkio arba abu
    Refactor student grade calculation and output options.

--- vpradine.cpp
+++ vpradine.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 using std::string;
 using std::vector;
@@ -9,19 +10,41 @@ using std::cout;
 using std::endl;
 using std::cin;
 using std::left;
-using std::right;
 using std::setw;
 using std::fixed;
 using std::setprecision;
+using std::sort;
+
 struct studentas
 {
     string vardas, pavarde;
     vector<int> paz;
     int exam;
-    double galutinis;
 };
 
-void printas(studentas A);
+double skaiciuotiVidurki(studentas A)
+{
+    double suma = 0;
+
+    for (int i = 0; i < A.paz.size(); i++)
+    {
+        suma += A.paz[i];
+    }
+
+    return suma / A.paz.size();
+}
+
+double skaiciuotiMediana(studentas A)
+{
+    sort(A.paz.begin(), A.paz.end());
+
+    int n = A.paz.size();
+
+    if (n % 2 == 1)
+        return A.paz[n / 2];
+    else
+        return (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
+}
 
 int main()
 {
@@ -29,9 +52,9 @@ int main()
     vector<studentas> grupe;
     studentas A;
 
-    std::cout << "Kiek studentu yra sarase: ";
+    cout << "Kiek studentu yra sarase: ";
     int n;
-    std::cin >> n;
+    cin >> n;
 
     for (int i = 0; i < n; i++)
     {
@@ -52,17 +75,6 @@ int main()
         cout << "Iveskite egzamino pazymi: ";
         cin >> A.exam;
 
-        double suma = 0;
-
-        for (int i = 0; i < A.paz.size(); i++)
-        {
-            suma += A.paz[i];
-        }
-
-        double vidurkis =suma/A.paz.size();
-
-        A.galutinis =0.4*vidurkis + 0.6*A.exam;
-
         grupe.push_back(A);
 
         A.vardas.clear();
@@ -70,19 +82,60 @@ int main()
         A.paz.clear();
     }
 
-    cout <<endl;
-    cout <<left <<setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis (Vid.)" <<endl;
-    cout << "---------------------------------------------\n";
+    int pasirinkimas;
+    cout<<"pasirinkite kaip norit atspausdint duomenis" << endl;
+    cout << endl; cout << "1 - Vidurkis" << endl; cout << "2 - Mediana" << endl; cout << "3 - Abu" << endl; cout << "Pasirinkite: "; cin >> pasirinkimas;
 
-    for (studentas B : grupe)
+    switch (pasirinkimas)
     {
-        printas(B);
+        case 1:
+            cout << endl;
+            cout << left << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis (Vid.)" << endl;
+            cout << "---------------------------------------------\n";
+
+            for (studentas B : grupe)
+            {
+                double vidurkis = skaiciuotiVidurki(B);
+                double galutinis = 0.4 * vidurkis + 0.6 * B.exam;
+
+                cout << left << setw(15) << B.pavarde << setw(15) << B.vardas << fixed << setprecision(2) << galutinis << endl;
+            }
+            break;
+
+        case 2:
+            cout << endl;
+            cout << left << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(15) << "Galutinis (Med.)" << endl;
+            cout << "---------------------------------------------\n";
+
+            for (studentas B : grupe)
+            {
+                double mediana = skaiciuotiMediana(B);
+                double galutinis = 0.4 * mediana + 0.6 * B.exam;
+
+                cout << left << setw(15) << B.pavarde << setw(15) << B.vardas << fixed << setprecision(2) << galutinis << endl;
+            }
+            break;
+
+        case 3:
+            cout << endl;
+            cout << left << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
+            cout << "-------------------------------------------------------------\n";
+
+            for (studentas B : grupe)
+            {
+                double vidurkis = skaiciuotiVidurki(B);
+                double mediana = skaiciuotiMediana(B);
+
+                double galutinisVid = 0.4 * vidurkis + 0.6 * B.exam;
+                double galutinisMed = 0.4 * mediana + 0.6 * B.exam;
+
+                cout << left << setw(15) << B.pavarde << setw(15) << B.vardas << setw(20) << fixed << setprecision(2) << galutinisVid << setw(20) << galutinisMed << endl;
+            }
+            break;
+
+        default:
+            cout << "Neegzistuojaa toks pasirinkimas." << endl;
     }
 
     return 0;
-}
-
-void printas(studentas A)
-{
-    cout << left << std::setw(15) << A.pavarde << setw(15) << A.vardas <<fixed <<setprecision(2) << A.galutinis << endl;
 }
